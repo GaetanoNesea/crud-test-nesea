ee1344c0679ca31d8f3ea67b3e870788f039f0a8
@Gaetano79 <gaetano.sabino@gmail.com>
2023-02-20 06:40:58 +0100

    fine test persone service

--- src/persone/persone.service.spec.ts
+++ src/persone/persone.service.spec.ts
@@ -5,6 +5,7 @@ import {IPersona} from './models/personeResponse.model';
 import {Observable} from 'rxjs';
 import {AxiosResponse} from 'axios';
 import {NotFoundException} from '@nestjs/common';
+import {UpdatePersoneDto} from './dto/update-persone.dto';
 
 describe('PersoneService', () => {
   let service: PersoneService;
@@ -12,6 +13,8 @@ describe('PersoneService', () => {
     Observable<AxiosResponse<IPersona[], any>>,
     []
   >;
+  let personaSet: UpdatePersoneDto;
+  let listaPersone: IPersona[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,23 +24,13 @@ describe('PersoneService', () => {
 
     service = module.get<PersoneService>(PersoneService);
     apiEsterna = jest.spyOn(service, 'getPersonaAPI');
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-  it('should lista persone vuota', function () {
-    expect(service.lista.length).toBe(0);
-  });
-  it('should lista vuota chiama api esterna e poi crea uuid nella lista', async () => {
-    //const spy = jest.spyOn(service, 'getPersonaAPI');
-    expect(service.lista.length).toBe(0);
-    await service.findAll();
-    expect(apiEsterna).toBeCalledTimes(1);
-    expect(service.lista).not.toHaveLength(0);
-  });
-  it('should con lista piena non chiama api esterna e nemmeno la funzione per uuid', async () => {
-    service.lista = [
+    personaSet = {
+      id: '',
+      name: 'Gaetano',
+      email: 'g.sabino',
+      username: 'sabino',
+    };
+    listaPersone = [
       {
         id: '1',
         email: 'g.sabino@nesea.it',
@@ -58,6 +51,23 @@ describe('PersoneService', () => {
         website: null,
       },
     ];
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  it('should lista persone vuota', function () {
+    expect(service.lista.length).toBe(0);
+  });
+  it('should lista vuota chiama api esterna e poi crea uuid nella lista', async () => {
+    //const spy = jest.spyOn(service, 'getPersonaAPI');
+    expect(service.lista.length).toBe(0);
+    await service.findAll();
+    expect(apiEsterna).toBeCalledTimes(1);
+    expect(service.lista).not.toHaveLength(0);
+  });
+  it('should con lista piena non chiama api esterna e nemmeno la funzione per uuid', async () => {
+    service.lista = listaPersone;
     await service.findAll();
     expect(apiEsterna).toBeCalledTimes(0);
     expect(service.lista).toHaveLength(1);
@@ -76,11 +86,9 @@ describe('PersoneService', () => {
       persona: service.lista.at(1),
     });
   });
-
   it('should remove persona con lista vuota NotfoundException', function () {
     expect(() => service.remove('')).toThrow(NotFoundException);
   });
-
   it('should remove persona con id sbagliato', async () => {
     // let res = service.findOne('');
     await service.findAll();
@@ -90,5 +98,19 @@ describe('PersoneService', () => {
     const res = service.remove(id);
     expect(service.lista).not.toContain(res.persona);
     expect(numeroPersone).toEqual(service.lista.length + 1);
+  });
+  it('should modifica persona con lista vuota oppure con id sbagliato Errore NotFoundException', function () {
+    expect(() => service.update('', personaSet)).toThrow(NotFoundException);
+    service.lista = listaPersone;
+    expect(() => service.update('2', personaSet)).toThrow(NotFoundException);
+    expect(() => service.update('1', personaSet)).not.toThrow(NotFoundException);
+  });
+  it('should modifica persona response con persona modificata e lista persona con persona modificata', function () {
+    service.lista = listaPersone;
+    let res = service.update('1', personaSet);
+    expect(res.persona.email).toEqual(personaSet.email);
+    expect(res.persona.username).toEqual(personaSet.username);
+    expect(res.persona.name).toEqual(personaSet.name);
+    expect(service.lista).toContain(res.persona);
   });
 });
