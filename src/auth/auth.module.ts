b440fd3a5080e8378c88c40eb695c557f278f1b3
@Gaetano79 <gaetano.sabino@gmail.com>
2023-02-25 09:43:24 +0100

    jwt implementato

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,19 @@ import {UsersModule} from '../users/users.module';
 import {UsersService} from '../users/users.service';
 import {PassportModule} from '@nestjs/passport';
 import {LocalStrategy} from './local.strategy';
+import {JwtModule} from '@nestjs/jwt';
+import {jwtConstant} from './constant';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstant.secret,
+      signOptions: {expiresIn: '1 days'},
+    }),
+  ],
   providers: [AuthService, UsersService, LocalStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,19 @@
 import {Injectable} from '@nestjs/common';
 import {UsersService} from '../users/users.service';
+import {JwtService} from '@nestjs/jwt';
+import {IUserModel} from '../users/models/user.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly serviceUser: UsersService) {}
+  constructor(
+    private readonly serviceUser: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async validaUser(name: string, password: string): Promise<any> {
+  async validaUser(
+    name: string,
+    password: string,
+  ): Promise<{id: number; username: string} | null> {
     const {pass, username, id} = await this.serviceUser.findOne(name);
     if (username && pass === password) {
       return {
@@ -14,5 +22,13 @@ export class AuthService {
       };
     }
     return null;
+  }
+
+  async login(user: IUserModel) {
+    const payload = {username: user.username, sub: user.id};
+    return {
+      access_token: this.jwtService.sign(payload),
+      token: this.jwtService.sign(payload),
+    };
   }
 }
