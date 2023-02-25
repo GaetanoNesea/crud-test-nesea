392bd8dca1fb3ed46f3194a5b3c5c65cd8a0d399
@Gaetano79 <gaetano.sabino@gmail.com>
2023-02-25 07:49:36 +0100

    stratey implementata

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import {Module} from '@nestjs/common';
 import {AuthService} from './auth.service';
 import {UsersModule} from '../users/users.module';
 import {UsersService} from '../users/users.service';
+import {PassportModule} from '@nestjs/passport';
+import {LocalStrategy} from './local.strategy';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService, UsersService],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, UsersService, LocalStrategy],
 })
 export class AuthModule {}
